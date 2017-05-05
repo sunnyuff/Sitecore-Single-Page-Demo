2d69ffc9c92047b1e75f80056e9814a73ce1745f
Sunny Jhunjhunwala <sunnyuff@gmail.com>
2017-05-05 12:48:46 +0400

    Initial Commit
    Added all files from Demo project.

---
+++ MVCDEMO/Website/Content/Js/mvc3.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+    'use strict';
+
+    if ($('html').hasClass('notexpeditor')) {
+        var $page = $('#main'),
+        options = {
+            debug: true,
+            prefetch: false,
+            //cacheLength:10,
+            onStart: {
+                duration: 250, // Duration of our animation
+                render: function ($container) {
+                    // Add your CSS animation reversing class
+                    $container.addClass('is-exiting');
+                    // Restart your animation
+                    smoothState.restartCSSAnimations();
+                }
+            },
+            onReady: {
+                duration: 0,
+                render: function ($container, $newContent) {
+                    // Remove your CSS animation reversing class
+                    $container.removeClass('is-exiting');
+                    // Inject the new content
+                    $container.html($newContent);
+                }
+            }
+        },
+        smoothState = $page.smoothState(options).data('smoothState');
+        $('.main-content').addClass('page-moveToLeft');
+    }
+    
+});
